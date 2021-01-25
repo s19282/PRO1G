3a5443728c551268e607f26594fc9ecffa3315c2
Mateusz <s19282@pjwstk.edu.pl>
2021-01-25 21:20:34 +0100

    z6 memorize

---
+++ cw02/src/z6.groovy
@@ -0,0 +1,26 @@
+def counter = 1
+def fib
+fib = { n ->
+    counter++
+    if (n < 2) {
+        n
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+}
+assert fib(15)
+println "Bez użycia memoize(): $counter"
+
+counter = 1
+fib = { n ->
+    counter++
+    if (n < 2) {
+        n
+    } else {
+        fib(n - 1) + fib(n - 2)
+    }
+}.memoize()
+
+assert fib(25)
+
+println "Z użyciem memoize(): $counter"
