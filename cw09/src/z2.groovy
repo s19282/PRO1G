929ed0cd9b6194f99e59c7be009caca9a33d5e15
Mateusz <s19282@pjwstk.edu.pl>
2021-01-25 19:01:05 +0100

    z2

---
+++ cw09/src/z2.groovy
@@ -0,0 +1,16 @@
+import groovy.swing.SwingBuilder
+import javax.swing.*
+import java.awt.*
+
+def colors = [Color.CYAN, Color.MAGENTA, Color.YELLOW, Color.BLACK]
+def c = 0
+
+def swing = new SwingBuilder().edt {
+    frame(title: 'ColoredButton', defaultCloseOperation: JFrame.EXIT_ON_CLOSE, pack: true, show: true) {
+        button(text: 'Zmień kolor', actionPerformed: { e ->
+            if (c == colors.size())
+                c = 0
+            e.source.background = colors[c++]
+        })
+    }
+}
